fe2daf3d3ca132636a605ad5017ac1056b05e1c8
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2021-03-02 15:40:07 -0300

    Fixed fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -3,14 +3,14 @@ import { IFetchParameters, IFetchReturn } from 'interfaces/FetchParameters';
 import { sessionCheck } from 'utils/session';
 
 const doFetch = async({ url, method, body }: IFetchParameters): Promise<IFetchReturn> => {
-    const rawResponse = await fetch('http://localhost:8000/' + url, {
+    const rawResponse = await fetch('https://to-do-list-backend-dr4kk0nnys.herokuapp.com/' + url, {
         method,
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(body)
-    })
+    });
     const content = await rawResponse.json();
     sessionCheck(content);
 
